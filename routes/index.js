24f10561ecc3e194a74e57a0ac00768f76c5d963
Luna <20017802+boppinluna@users.noreply.github.com>
2024-04-17 18:28:22 +0100

    init

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
+const ytdl = require('ytdl-core');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
